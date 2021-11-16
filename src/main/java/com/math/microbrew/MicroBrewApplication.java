4c112491af62f6ae47bcb6ea6d3df800de993095
Matheesh <matheeshprabhu123@gmail.com>
2021-11-16 17:04:25 +0530

    Initial Commit

---
+++ src/main/java/com/math/microbrew/MicroBrewApplication.java
@@ -0,0 +1,13 @@
+package com.math.microbrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroBrewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroBrewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/math/microbrew/MicroBrewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.math.microbrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroBrewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
